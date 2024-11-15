461f74881c55b5def89428a4999593052b914d37
Danial Ghofrani <danialghofrani2004@gmail.com>
2024-11-15 14:31:13 +0330

    Standard Scaling and fitting the data.

--- assignments/Florida_weather/flood_model.py
+++ assignments/Florida_weather/flood_model.py
@@ -9,22 +9,28 @@ import numpy as np
 
 df = pd.read_csv("final_flood_data.csv")
 # There are some Nan rows. We need to replace or drop them, so we don't get any error during fit.
-print(df.isna().any())
-print(df.isna().sum())
-print(df[(df["snow_depth"].isna()) & (df["flood"] == 1 )].shape[0])
+# print(df.isna().any())
+# print(df.isna().sum())
+# print(df[(df["snow_depth"].isna()) & (df["flood"] == 1 )].shape[0])
 # There are 480 missing data and only 47 rows belong to the data with flood=1, so we can drop them with no concern.
 df = df.dropna()
 print(df.isna().sum())
 
 
+scaler = StandardScaler()
+
+
 X = df.drop(columns="flood")
+X = scaler.fit_transform(X)
 y = df["flood"]
+
+
 # print(np.unique(y))
 
 
 
 x_train, x_test, y_train, y_test = train_test_split(X, y,test_size=0.2, shuffle=True, stratify=y, random_state=23)
-LR_model = LogisticRegression()
+LR_model = LogisticRegression(max_iter=100)
 LR_model.fit(x_train, y_train)
 
 ## evaluation:
